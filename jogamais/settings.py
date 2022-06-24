13a0bc86735e2bd95f9cb5390175580a92edbe76
Renato de Medeiros Tranchesi <renatodmt@gmail.com>
2022-06-24 16:10:40 -0300

    Changed allowed hosts settings

--- jogamais/settings.py
+++ jogamais/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hem(v0yhwsp^*zx!ed#es9-g!c%5go!_nfxr_cbqfnx1uwyy!i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['renatodmt.pythonanywhere.com']
 
 
 # Application definition
